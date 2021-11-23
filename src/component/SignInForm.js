1ae1deb7e396c6c1a65a9ab37358bc72a072e06f
aristides1000 <aristides1000@gmail.com>
2021-11-23 18:25:34 -0400

    Add styles in nav-bar component and in the forms

--- src/component/SignInForm.js
+++ src/component/SignInForm.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { signIn } from '../redux/user/user';
+import '../css/SignInForm.css';
 
 const SignInForm = () => {
   const dispatch = useDispatch();
@@ -15,43 +16,47 @@ const SignInForm = () => {
   const [password, setPassword] = useState('');
 
   return (
-    <Form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        await signInAction(email, password);
-        setEmail('');
-        setPassword('');
-        navigate('/');
-      }}
-    >
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
+    <div className="row bg-image-sign-in mx-0 d-flex flex-column justify-content-center align-items-center">
+      <div className="col-sm-6">
+        <h2 className="light-font">Sign In</h2>
+        <Form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            await signInAction(email, password);
+            setEmail('');
+            setPassword('');
+            navigate('/');
           }}
-          placeholder="Enter email"
-        />
-        <Form.Text className="text-muted">
-          We&apos;ll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+        >
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              placeholder="Enter email"
+              className="input-submit"
+            />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          placeholder="Password"
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              placeholder="Password"
+              className="input-submit"
+            />
+          </Form.Group>
+          <Button type="submit" className="button-submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 };
 
--- src/component/SignUpForm.js
+++ src/component/SignUpForm.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { signUp } from '../redux/user/user';
+import '../css/SignUpForm.css';
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
   const signUpAction = bindActionCreators(signUp, dispatch);
-  const user = useSelector((state) => state.user);
   const navigate = useNavigate();
 
   const [email, setEmail] = useState('');
@@ -18,67 +18,71 @@ const SignUpForm = () => {
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
 
   return (
-    <Form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        await signUpAction(userName, email, password, passwordConfirmation);
-        setEmail('');
-        setPassword('');
-        setUserName('');
-        setPasswordConfirmation('');
-        navigate('/');
-      }}
-    >
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
+    <div className="row bg-image-sign-up mx-0 d-flex flex-column justify-content-center align-items-center">
+      <div className="col-sm-6">
+        <h2 className="light-font">Sign Up</h2>
+        <Form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            await signUpAction(userName, email, password, passwordConfirmation);
+            setEmail('');
+            setPassword('');
+            setUserName('');
+            setPasswordConfirmation('');
+            navigate('/');
           }}
-          placeholder="Enter email"
-        />
-        <Form.Text className="text-muted">
-          We&apos;ll never share your email with anyone else.
-          {' '}
-          {user.name}
-        </Form.Text>
-      </Form.Group>
+        >
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              placeholder="Enter email"
+              className="input-submit"
+            />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Control
-          type="text"
-          value={userName}
-          onChange={(e) => {
-            setUserName(e.target.value);
-          }}
-          placeholder="User Name"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          placeholder="Password"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control
-          type="password"
-          value={passwordConfirmation}
-          onChange={(e) => {
-            setPasswordConfirmation(e.target.value);
-          }}
-          placeholder="Password Confirmation"
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Control
+              type="text"
+              value={userName}
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+              placeholder="User Name"
+              className="input-submit"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              placeholder="Password"
+              className="input-submit"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              type="password"
+              value={passwordConfirmation}
+              onChange={(e) => {
+                setPasswordConfirmation(e.target.value);
+              }}
+              placeholder="Password Confirmation"
+              className="input-submit"
+            />
+          </Form.Group>
+          <Button type="submit" className="button-submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 };
 
