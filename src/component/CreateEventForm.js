8fa5ca06ea9a6f22e6ac75ffaf7b8e38938987a2
Enio N. de Souza <enionsouza@gmail.com>
2021-11-24 09:37:01 -0400

    style: Add styles to Create Event Form

--- src/component/CreateEventForm.js
+++ src/component/CreateEventForm.js
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { createEvent } from '../redux/event/event';
+import '../css/CreateEventForm.css';
 
 const CreateEventForm = () => {
   const dispatch = useDispatch();
@@ -14,56 +15,63 @@ const CreateEventForm = () => {
   const [eventImage, setEventImage] = useState('');
 
   return (
-    <Form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('name', eventName);
-        formData.append('description', eventDescription);
-        formData.append('image', eventImage);
-        await createEventAction(formData);
-        setEventName('');
-        setEventDescription('');
-        e.target.reset();
-      }}
-      className="mt-3"
-    >
-      <Form.Group className="mb-3" controlId="formBasicEventName">
-        <Form.Control
-          type="text"
-          value={eventName}
-          onChange={(e) => {
-            setEventName(e.target.value);
+    <div className="row bg-image-sign-up mx-0 d-flex flex-column justify-content-center align-items-center">
+      <div className="col-sm-6">
+        <h2 className="light-font">Create new event</h2>
+        <Form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const formData = new FormData();
+            formData.append('name', eventName);
+            formData.append('description', eventDescription);
+            formData.append('image', eventImage);
+            await createEventAction(formData);
+            setEventName('');
+            setEventDescription('');
+            e.target.reset();
           }}
-          placeholder="Enter event name"
-        />
-      </Form.Group>
+        >
+          <Form.Group className="mb-3" controlId="formBasicEventName">
+            <Form.Control
+              type="text"
+              value={eventName}
+              onChange={(e) => {
+                setEventName(e.target.value);
+              }}
+              placeholder="Enter event name"
+              className="input-submit"
+            />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEventDescription">
-        <Form.Control
-          as="textarea"
-          rows={3}
-          value={eventDescription}
-          onChange={(e) => {
-            setEventDescription(e.target.value);
-          }}
-          placeholder="Enter event Description"
-        />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEventDescription">
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={eventDescription}
+              onChange={(e) => {
+                setEventDescription(e.target.value);
+              }}
+              placeholder="Enter event Description"
+              className="input-textarea"
+            />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEventImage">
-        <Form.Control
-          type="file"
-          onChange={(e) => {
-            setEventImage(e.target.files[0]);
-          }}
-        />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEventImage">
+            <Form.Control
+              type="file"
+              onChange={(e) => {
+                setEventImage(e.target.files[0]);
+              }}
+              className="input-submit"
+            />
+          </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Button type="submit" className="button-submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 };
 
